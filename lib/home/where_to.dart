91fcb8123eecc68a4903a7a789d638bef06fc424
imran <imran40031@gmail.com>
2021-09-25 23:53:39 +0600

    final commit

--- lib/home/where_to.dart
+++ lib/home/where_to.dart
@@ -7,51 +7,93 @@ class WhereTo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.white,
-            leading: Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  child: SvgPicture.asset(
-                    'assets/svg/user.svg',
-                    height: 25,
-                    width: 25,
-                  ),
-                ),
-                Text(
-                  'For Me',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-                Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black,
-                ),
-              ],
-            ),
-            centerTitle: true,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
           ),
-          body: Column(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.transparent,
+                child: SvgPicture.asset(
+                  'assets/svg/user.svg',
+                  height: 25,
+                  width: 25,
+                ),
+              ),
+              Text(
+                'For Me',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              Icon(
+                Icons.arrow_drop_down,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
             children: [
               SizedBox(height: 10),
-              Container(
-                width: double.infinity,
-                height: 30,
-                decoration: BoxDecoration(color: Colors.black),
-              )
+              IconText(
+                text: 'Saved Place',
+                icon: Icons.star,
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              IconText(
+                text: 'Set Location on Map',
+                icon: Icons.pin_drop,
+              ),
+              SizedBox(height: 20),
+              IconText(
+                text: 'Enter Destination Later',
+                icon: Icons.skip_next,
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class IconText extends StatelessWidget {
+  const IconText({
+    Key? key,
+    this.icon,
+    this.text,
+  }) : super(key: key);
+  final IconData? icon;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: EdgeInsets.all(5),
+          height: 40,
+          width: 40,
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Color(0xFFEEEFEF)),
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(width: 10),
+        Text(text!),
+      ],
     );
   }
 }
